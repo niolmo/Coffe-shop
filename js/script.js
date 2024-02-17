86ce030b59e771f1736db72f3d93bf9d1bf29a75
niolmo <71871013+niolmo@users.noreply.github.com>
2024-02-17 20:42:53 +0500

    mob-menu

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 // Мобильное меню
+
+const menu = document.querySelector(".bx-menu");
+const burger = document.querySelector(".header__burger");
+const close = document.querySelector(".header__burger_close");
+
+menu.addEventListener("click", () => {
+  burger.classList.toggle("header__burger_active");
+});
+
+close.addEventListener("click", () => {
+  if (burger.classList.contains("header__burger_active")) {
+    burger.classList.remove("header__burger_active");
+  }
+});
